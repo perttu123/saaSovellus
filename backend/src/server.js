895488cecb1779b70401e9629596503ed7441926
perttu123 <epeva005@edu.xamk.fi>
2024-02-20 14:08:43 +0200

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,11 @@
-//asdksokd
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.listen(port, ()=>{
+    console.log("listening port: "+port);
+});
