78f23f956a1ea3bcf00bb56757a50dbf428a7d91
perttu123 <epeva005@edu.xamk.fi>
2024-03-05 13:53:27 +0200

    fetchi
    Toimii

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.get('/data', async (req, res)=>{
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=64&longitude=26&hourly=temperature_2m&timezone=Europe%2FBerlin');
+    const data = await response.json();
+    console.log(data);
+    res.send(data);
+});
+
 app.listen(port, ()=>{
     console.log("listening port: "+port);
 });
