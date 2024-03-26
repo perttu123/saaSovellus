f5afdd121372a66f6da530ccad69c6739a149976
perttu123 <epeva005@edu.xamk.fi>
2024-03-26 14:26:59 +0200

    Backend ei cräshää, jos tekee kirjoitusvirheen.

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -9,32 +9,43 @@ router.get('/data/:search', async (req, res)=>{
     const search = req.params.search;
 
     const coordinates = await getCoordinates(search);
-
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Africa%2FCairo`);
     
-    const data = await response.json();
 
-    const sevenHours = get7Hours({data});
-    const weekly = getWeeklyData({data});
-    const weatherData = {
-        hours: sevenHours,
-        current: data.current,
-        daily: weekly,
-        city: search
+    if(coordinates=="error"){
+        console.log("okfadga");
+        res.send({error: "error"});
     }
-    res.send(weatherData);
+    else{
+        
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Africa%2FCairo`);
+        const data = await response.json();
 
+        const sevenHours = get7Hours({data});
+        const weekly = getWeeklyData({data});
+        const weatherData = {
+            hours: sevenHours,
+            current: data.current,
+            daily: weekly,
+            city: search
+        }
+        res.send(weatherData);
+    }    
 });
 
 async function getCoordinates(search){
     
     const response2 = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${search}`)
-
     const data = await response2.json();
+    if(data && data.length === 0){
 
-    const coordinates = {latitude: data[0].lat, longitude: data[0].lon}
-        
-    return coordinates;
+        return "error";
+    }
+    else{
+        const coordinates = {latitude: data[0].lat, longitude: data[0].lon}
+        return coordinates;
+    }
+   
+    
     
 }
 
