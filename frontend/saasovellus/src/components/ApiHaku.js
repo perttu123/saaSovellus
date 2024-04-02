4aba54b8a02fb51a9c5ee13ba56ed906cdf52148
perttu123 <epeva005@edu.xamk.fi>
2024-04-02 14:16:44 +0300

    fwefwef

--- frontend/saasovellus/src/components/ApiHaku.js
+++ frontend/saasovellus/src/components/ApiHaku.js
@@ -1,13 +1,14 @@
 
 
-export async function ApiHaku(){
-    const response = await fetch("http://localhost:5000/data");
-    const data = await response.json();
-    return data;
-}
-
 export async function SendLocation(search) {
     const response = await fetch(`http://localhost:5000/data/${search}`);
     const data = await response.json();
     return data;
 }
+
+export async function Vertailu(search){
+    const response = await fetch(`http://localhost:5000/vertaile/${search.kaupunki1}vs${search.kaupunki2}`);
+    const data = await response.json();
+    return data;
+}
+
